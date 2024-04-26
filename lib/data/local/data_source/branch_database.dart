d0e51522774ede65195a4726ebcb840653eadf08
Joe15975 <43861205+Joe15975@users.noreply.github.com>
2024-04-26 22:48:35 +0300

    fixed to be compatible with widows

--- lib/data/local/data_source/branch_database.dart
+++ lib/data/local/data_source/branch_database.dart
@@ -1,7 +1,11 @@
 
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart' as DesktopSQLite;
+import 'package:sqflite/sqflite.dart' as SQFLite;
 
 import '../../../app/consts.dart';
 import '../../../domain/entities/branch.dart';
@@ -11,7 +15,7 @@ abstract class BranchDatabaseSource {
 
   factory BranchDatabaseSource() => _BranchDatabase();
 
-  static Database? get database => _BranchDatabase.database;
+  static SQFLite.Database? get database => _BranchDatabase.database;
 
   Future<void> initializeDB();
   Future<bool> createTable();
@@ -22,19 +26,61 @@ abstract class BranchDatabaseSource {
 
 class _BranchDatabase implements BranchDatabaseSource {
 
-  static Database? database;
+  static SQFLite.Database? database;
 
   @override
   Future<void> initializeDB() async {
 
-    final dbPath = await getDatabasesPath();
 
-    final path = join(dbPath, '${Consts.dbNames.branch}.db');
+    String path = '';
+
+    String appDocDir = join((await getApplicationDocumentsDirectory()).path, 'databases', );
+
+
+
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+
+      await _openDBDesktop(appDocDir);
+
+    } else {
+      await _openDBMobile(appDocDir);
+    }
+
+
+  }
+
+  Future<void> _openDBDesktop(String dir) async {
+    DesktopSQLite.sqfliteFfiInit();
+    DesktopSQLite.databaseFactory = DesktopSQLite.databaseFactoryFfi;
+    final context = Context(style: Style.windows);
+
+    final path = context.join(dir, '${Consts.dbNames.branch}.db');
 
     try {
-      database = await openDatabase(
+      database = await DesktopSQLite.openDatabase(
         path,
-        version: 1,
+        // version: 1,
+      );
+
+      if (! await checkTableExists(database!, Consts.tableNames.branch)) {
+        createTable();
+      }
+
+    } catch (e, s) {
+      if (kDebugMode){
+        debugPrintStack(stackTrace: s, label: e.toString());
+      }
+    }
+  }
+
+  Future<void> _openDBMobile(String dir) async {
+
+    final path = join(dir, '${Consts.dbNames.branch}.db');
+
+    try {
+      database = await SQFLite.openDatabase(
+        path,
+        // version: 1,
       );
 
       if (! await checkTableExists(database!, Consts.tableNames.branch)) {
@@ -93,12 +139,14 @@ class _BranchDatabase implements BranchDatabaseSource {
     throw UnimplementedError();
   }
 
-  Future<bool> checkTableExists(Database database, String branch) async {
+  Future<bool> checkTableExists(SQFLite.Database database, String branch) async {
     try{
-      int? count = Sqflite
-          .firstIntValue(await database.rawQuery('SELECT COUNT(*) FROM $branch'));
+      // int? count = Sqflite
+      //     .firstIntValue(await database.rawQuery('SELECT COUNT(*) FROM $branch'));
 
-      return (count ?? 0) > 0;
+      int? count = (await database.query(branch)).length;
+
+      return (count ?? 0) >= 0;
     } catch (e, s) {
       if (kDebugMode) {
         print('Could find table');
